2a32b52dd45a1351590e6818eeccefc278cea83d
gpk1995 <37920840+gpk1995@users.noreply.github.com>
2018-04-11 19:16:01 +0530

    Add files via upload

---
+++ April11/prob1.py
@@ -0,0 +1,34 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('employee_data.csv', sep=',')
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+dataset['Department'] = labelencoder.fit_transform(dataset['Department'])
+
+X = np.array(dataset.iloc[:, :-1].values).reshape(-1, 4)
+y = np.array(dataset.iloc[:, 4].values).reshape(-1, 1)
+
+# Avoiding the Dummy Variable Trap
+#X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
